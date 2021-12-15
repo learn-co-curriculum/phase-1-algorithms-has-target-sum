e3c6ba5423e3ce9c89baff07eaae8a02638465de
Kyle Goodwin <kyle.goodwin93@gmail.com>
2021-12-15 13:13:58 -0500

    commit tree a6bb74237fea278f296667378094a9c30b1af28a

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length; i++){
+      const complement = target - array[i];
+      for (let j = i + 1; j < array.length; j++){
+        if(array[j] === complement){
+          return true
+        }
+      }
+  }
+  return false
 }
+
+
 
 /* 
   Write the Big O time complexity of your function here
+
+  i think n^2
 */
 
 /* 
-  Add your pseudocode here
+  function hasTargetSum takes an array and target
+  check that for each number of array, if, when it added to each of the remaining three numbers, it is equal to target 
+  if yes, return true
+  else return false 
 */
 
 /*
-  Add written explanation of your solution here
+  I will be writing a function hasTargetSum that takes an array and number as arguments. 
+  The function will test to see if any two numbers of the array will sum to equal the target value. if so it will return true, else false. 
 */
 
 // You can run `node index.js` to view these console logs
@@ -29,6 +45,12 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([1, 8, 9, 10], 11))
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([1, 8, 7, 5, 2], 2))
 }
 
 module.exports = hasTargetSum;
