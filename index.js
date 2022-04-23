b51695f94cb1108d65e9f4b1af6bb8d775525798
Elijah Mbugi <mukundielijah@gmail.com>
2022-04-23 14:13:12 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+
+  for (let i = 0; i < array.length; i++) {
+    let difference = target - array[i];
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] === difference) return true;
+    }
+  }
+
+  return false;
 }
 
 /* 
@@ -12,11 +21,16 @@ function hasTargetSum(array, target) {
 
 /*
   Add written explanation of your solution here
+  the function receives an array and a target as arguments.
+  If the sum of any 2 element pair ===target, the function returns true.
+  Otherwise it returns false
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  //console.log(hasTargetSum([1,2,3,4],5));----true
+  // console.log(hasTargetSum([1,2,3,4],1));----false
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
